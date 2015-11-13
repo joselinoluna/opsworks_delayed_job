de4561e91363c6675c67ba743bd5c1fd3daea393
Joe-e Luna <jluna@trusted-name.com>
2015-11-13 19:41:34 +0800

    * Include cron task setup to deploy

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -46,5 +46,4 @@ node[:deploy].each do |application, deploy|
     code "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application} --roles #{layers.join(',')}"
     only_if "cd #{deploy[:deploy_to]}/current && bundle show whenever"
   end
-
 end
